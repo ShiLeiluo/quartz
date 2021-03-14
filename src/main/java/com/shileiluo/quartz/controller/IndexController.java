5ebaee54e721fa6b9b62ba372bfedff2e03f67d2
ShiLeiluo <1549014363@qq.com>
2021-03-14 18:32:54 +0800

    0

---
+++ src/main/java/com/shileiluo/quartz/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.shileiluo.quartz.controller;
+
+/**
+ * @description: TODO
+ * @author: ShiLeiluo
+ * @date: 2021/3/14 18:29
+ * @version: v1.0
+ */
+
+public class IndexController {
+}
---
+++ src/main/java/com/shileiluo/quartz/controller/JobController.java
@@ -0,0 +1,11 @@
+package com.shileiluo.quartz.controller;
+
+/**
+ * @description: TODO
+ * @author: ShiLeiluo
+ * @date: 2021/3/14 18:32
+ * @version: v1.0
+ */
+
+public class JobController {
+}
