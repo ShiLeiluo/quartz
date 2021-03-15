fb8c08f94218564e2f76e9c52dd072875c73b548
ShiLeiluo <1549014363@qq.com>
2021-03-15 21:31:43 +0800

    0

--- src/main/java/com/shileiluo/quartz/controller/IndexController.java
+++ src/main/java/com/shileiluo/quartz/controller/IndexController.java
@@ -1,5 +1,16 @@
 package com.shileiluo.quartz.controller;
 
+import com.shileiluo.quartz.model.ScheduleJob;
+import com.shileiluo.quartz.service.SchedulerJobService;
+import org.quartz.Scheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 /**
  * @description: TODO
  * @author: ShiLeiluo
@@ -8,4 +19,22 @@ package com.shileiluo.quartz.controller;
  */
 
 public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @Autowired
+    private SchedulerJobService schedulerJobService;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        logger.info("[JobController] the url path:------------/index----------------");
+        logger.info("[JobController] the method index is start......");
+        List<ScheduleJob> jobList = schedulerJobService.getAllScheduleJob();
+        model.addAttribute("jobs",jobList);
+        System.out.println(jobList);
+        logger.info("[JobController] the method index is end......");
+        return "index";
+    }
 }
